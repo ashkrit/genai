0f669e1ebac102eb506b952da93fab9e84d5d07c
ashkrit@gmail.com <ashkrit@gmail.com>
2024-05-09 00:07:56 +0800

    Local model example

---
+++ llm/ollama_api.py
@@ -0,0 +1,40 @@
+import json
+import requests
+
+
+def ask(model:str,message:[]) -> str:
+    url = "http://0.0.0.0:11434/api/chat"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "model": model,
+        "messages":message,
+        "stream":True
+    }
+    reply = requests.post(url, headers=headers, data=json.dumps(data))
+    reply.raise_for_status()
+
+    buffer=""
+
+    for line in reply.iter_lines():
+       body = json.loads(line)
+       message = body["message"]
+       buffer+=message["content"]
+    
+    return buffer
+
+if __name__ == "__main__":
+    messages = []
+
+    while True:
+        question = input("Ask me something >: ")
+        if question == "exit":
+            break
+        messages.append({"role": "user", "content": question})
+        print("Reply >:")
+        reply = ask("llama3", messages)
+        print(reply)
+        messages.append({"role": "assistant", "content": reply})
+    
+
